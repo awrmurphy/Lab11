2721811b3710d68385461aab77dc79c0f7ce9aa1
awrmurphy <awrmurphy@gmail.com>
2025-08-11 17:11:15 -0400

    housekeeping and comments

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const { indexOf } = require("lodash");
 
 class ProductProperties{
     constructor(name,price,quantity){
@@ -34,17 +33,19 @@ class ProductProperties{
             this.inventory = array;
         }
         addProduct(product){
+            //it push
             this.inventory.push(product);
         }
         getInventoryValue(){
+            //adds the value of each item multiplied by quantity of that item to a counter variable
             var total=0;
             this.inventory.forEach(element =>{
                 total = (total + (element.price*element.quantity));
             })
             return total;
         }
-        findProduct(name){
-           // this.inventory.find(name);
+        findProduct(name){ 
+           // searches the store to find where the item you are looking for is and returns all the details about that item
            var i;
            this.inventory.forEach(el =>{
             
@@ -80,13 +81,13 @@ class ProductProperties{
     let Pop = new ProductProperties('Coke',1.79,200);
     let Candy = new ProductProperties('Reese\'s',1.50,200);
 
-    let store = new StoreInventory([Soup,Pop,Candy]);
-    store.addProduct(Apples);
-    store.addProduct(Oranges);
-    console.log(store);
-    console.log("Before Discount: \n"+store.getInventoryValue().toFixed(2));
-    ProductProperties.applyDiscount(store.inventory,0.15);
-    console.log("After Discount: \n"+store.getInventoryValue().toFixed(2));
-    console.log(store.findProduct('Apple'));
+    let store = new StoreInventory([Soup,Pop,Candy]); //opening store
+    store.addProduct(Apples); //stocking apples
+    store.addProduct(Oranges); //stocking oranges
+    console.log(store); //the store
+    console.log("Before Discount: \n"+store.getInventoryValue().toFixed(2)); //total inventory value before sale
+    ProductProperties.applyDiscount(store.inventory,0.15); //applying the sale
+    console.log("After Discount: \n"+store.getInventoryValue().toFixed(2)); //total inventory value after the sale
+    console.log(store.findProduct('Apple')); //searching the store for an apple
     
     
